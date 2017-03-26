23dee9ef67ebcbd1bfec9201ec4548414053dff9
MissGrass <1209784760@qq.com>
2017-03-26 13:24:31 +0800

    my first code

---
+++ 3.10.2.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+	char cChar1;
+	char cChar2;
+	char cChar3;
+	char cChar4;
+	char cChar5;
+	char cChar6;
+	char cChar7;
+	char cChar8;
+	char cChar9;
+	cChar1='F';
+	cChar2='i';
+	cChar3='n';
+	cChar4='e';
+	cChar5=' ';
+	cChar6='D';
+	cChar7='a';
+	cChar8='y';
+	cChar9='!';
+	
+	printf("%c",cChar1);
+	printf("%c",cChar2);
+	printf("%c",cChar3);
+	printf("%c",cChar4);
+	printf("%c",cChar5);
+	printf("%c",cChar6);
+	printf("%c",cChar7);
+	printf("%c",cChar8);
+	printf("%c",cChar9);
+	return 0;
+
+ } 
