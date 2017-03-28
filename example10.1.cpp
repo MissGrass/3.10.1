fb6f24b45eef95a587ee05616ba69253f08ff6fe
MissGrass <1209784760@qq.com>
2017-03-28 22:14:56 +0800

    2017-3-28

---
+++ example10.1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	int *ipointer1,*ipointer2;
+	scanf("%d,%d",&a,&b);
+	ipointer1=&a;
+	ipointer2=&b;
+	printf("The number is:%d,%d\n",*ipointer1,*ipointer2);
+}
---
+++ example10.4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	long i;
+	long *p;
+	printf("please input the number:\n");
+	scanf("%ld",&i);
+	p=&i;
+	printf("the result1 is:%ld\n",*&i);
+	printf("the result2 is:%ld\n",i);
+	printf("the result3 is:%ld\n",*p);
+ } 
